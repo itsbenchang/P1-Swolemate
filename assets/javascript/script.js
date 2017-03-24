85ebe37c08a8b78359f50ab29010fccbc9b0dba5
sharloteean <sharloteean@gmail.com>
2017-03-23 19:42:38 -0700

    Sign up functionality works, created links to other pages, added icons

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,19 +1,4 @@
 
-$("#login-button").on("click", function(){
-	$("#login").css("display", "block");
-	$("#login").css("width", "auto");
-});
-
-$("#signup-button").on("click", function(){
-	$("#signup").css("display", "block");
-	$("#signup").css("width", "auto");
-});
-
-$("#submit-login").on("click", function(){
-	console.log("click");
-	$(location).attr("href","profile.html");
-	return false;
-});
 
 $('button').mouseup(function() { this.blur() })
 
@@ -48,8 +33,191 @@ $(function () {
    messagingSenderId: "771999306115"
  };
 
+ firebase.initializeApp(config);
 
 
+ function toggleSignIn() {
+      if (firebase.auth().currentUser) {
+        // [START signout]
+        firebase.auth().signOut();
+        // [END signout]
+      } else {
+        var email = document.getElementById('email').value;
+        var password = document.getElementById('password').value;
+        if (email.length < 4) {
+          alert('Please enter an email address.');
+          return;
+        }
+        if (password.length < 4) {
+          alert('Please enter a password.');
+          return;
+        }
+        // Sign in with email and pass.
+        // [START authwithemail]
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // [START_EXCLUDE]
+          if (errorCode === 'auth/wrong-password') {
+            alert('Wrong password.');
+          } else {
+            alert(errorMessage);
+          }
+          console.log(error);
+          document.getElementById('login-button').disabled = false;
+          // [END_EXCLUDE]
+        });
+        // [END authwithemail]
+      }
+      document.getElementById('login-button').disabled = true;
+    }
+
+
+    function handleSignUp() {
+      var email = document.getElementById('email').value;
+      var password = document.getElementById('password').value;
+      if (email.length < 4) {
+        alert('Please enter an email address.');
+        return;
+      }
+      if (password.length < 4) {
+        alert('Please enter a password.');
+        return;
+      }
+      // Sign in with email and pass.
+      // [START createwithemail]
+      firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // [START_EXCLUDE]
+        if (errorCode == 'auth/weak-password') {
+          alert('The password is too weak.');
+        } else {
+          alert(errorMessage);
+        }
+        console.log(error);
+        // [END_EXCLUDE]
+      });
+      // [END createwithemail]
+    }
+
+    function sendPasswordReset() {
+      var email = document.getElementById('email').value;
+      // [START sendpasswordemail]
+      firebase.auth().sendPasswordResetEmail(email).then(function() {
+        // Password Reset Email Sent!
+        // [START_EXCLUDE]
+        alert('Password Reset Email Sent!');
+        // [END_EXCLUDE]
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // [START_EXCLUDE]
+        if (errorCode == 'auth/invalid-email') {
+          alert(errorMessage);
+        } else if (errorCode == 'auth/user-not-found') {
+          alert(errorMessage);
+        }
+        console.log(error);
+        // [END_EXCLUDE]
+      });
+      // [END sendpasswordemail];
+    }
+function initApp() {
+      // Listening for auth state changes.
+      // [START authstatelistener]
+      firebase.auth().onAuthStateChanged(function(user) {
+        // [START_EXCLUDE silent]
+        // document.getElementById('quickstart-verify-email').disabled = true;
+        // [END_EXCLUDE]
+        if (user) {
+          // User is signed in.
+          var displayName = user.displayName;
+          var email = user.email;
+          var emailVerified = user.emailVerified;
+          var photoURL = user.photoURL;
+          var isAnonymous = user.isAnonymous;
+          var uid = user.uid;
+          var providerData = user.providerData;
+          // [START_EXCLUDE]
+          //document.getElementById('login-button').textContent = 'Sign out';
+          // [END_EXCLUDE]
+        } else {
+          // User is signed out.
+          // [START_EXCLUDE]
+          document.getElementById('login-button').textContent = 'Log in';
+          // document.getElementById('quickstart-account-details').textContent = 'null';
+          // [END_EXCLUDE]
+        }
+        // [START_EXCLUDE silent]
+        document.getElementById('login-button').disabled = false;
+        // [END_EXCLUDE]
+      });
+      // [END authstatelistener]
+      document.getElementById('submit-login').addEventListener('click', toggleSignIn, false);
+      document.getElementById('submit-signup').addEventListener('click', handleSignUp, false);
+      //document.getElementById('quickstart-password-reset').addEventListener('click', sendPasswordReset, false);
+    }
+    window.onload = function() {
+      initApp();
+    };
+
+
+$("#login-button").on("click", function(){
+	$("#login").css("display", "block");
+	$("#login").css("width", "auto");
+	$("#signup-button").prop("disabled",true);
+	firebase.auth().onAuthStateChanged(user => {
+    if(user) {
+      window.location = 'profile.html';
+      }
+    });
+});
+
+$("#signup-button").on("click", function(){
+	$("#signup").css("display", "block");
+	$("#signup").css("width", "auto");
+	$("#login-button").prop("disabled",true);
+	firebase.auth().onAuthStateChanged(user => {
+    if(user) {
+      window.location = 'profile.html';
+      }
+    });
+});
+
+$("#submit-login").on("click", function(){
+	firebase.auth().onAuthStateChanged(user => {
+    if(user) {
+      window.location = 'profile.html';
+      }
+    });
+	console.log("click login");
+	toggleSignIn();
+});
+
+$("#submit-signup").on("click", function(){
+  firebase.auth().onAuthStateChanged(user => {
+    if(user) {
+      window.location = 'settings.html';
+      }
+    });
+  console.log("click signup");
+  handleSignUp();
+});
+
+$("#sign-out").on("click", function(){
+      if (firebase.auth().currentUser) {
+        // [START signout]
+        console.log("click");
+        firebase.auth().signOut();
+        window.location = 'index.html';
+        // [END signout]
+      }
+
+});
 
 $(document).ready(function() {
 
